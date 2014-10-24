02247fa28d9b516cdfc15fb2434e7e132e71de66
Joe Nelson <cred+github@begriffs.com>
2014-10-24 14:07:51 -0700

    New constructors, tests, docs, and travis config

--- Data/Aeson/Transform.hs
+++ Data/Aeson/Transform.hs
@@ -19,35 +19,43 @@ import qualified Data.HashMap.Strict as H
 -- output at various nodes in the tree.
 --
 data Builder =
-    Id                -- ^ Pass input directly as output
-  | At Text Builder   -- ^ Move to value in current object
-  | Attr Text         -- ^ Get value in current object
-  | Attrs [Text]      -- ^ Filter current object by keys
-  | Map Builder       -- ^ Map over input array
-  | Index Int Builder -- ^ Get value at index of current array
+    Id                  -- ^ Pass input directly as output
+  | At Text Builder     -- ^ Move to value in current object
+  | Attr Text           -- ^ Get value in current object
+  | Keep [Text]         -- ^ Filter current object by keys
+  | Map Builder         -- ^ Map over input array
+  | Index Int           -- ^ Get value at index of current array
+  | AtIndex Int Builder -- ^ Move to index in current array
   | Obj (H.HashMap Text Builder) -- ^ Produce object with given keys
+  | Merge Builder Builder  -- ^ Combine two objects
+
 
 -- | Generates new Aeson 'Value' guided by a 'Builder'
 --
-transform :: Builder -> Value -> Value
-transform (At k b)    (A.Object o) = transform b $ o H.! k
-transform (At _ _)    _            = error "Expecting object (At)"
-transform (Index i b) (Array a)    = transform b $ a Vec.! i
-transform (Index _ _) _            = error "Expecting array (Index)"
-transform (Map b)     (Array a)    = Array $ Vec.map (transform b) a
-transform (Map _)     _            = error "Expecting array (Map)"
-transform (Attr k)    (A.Object o) = o H.! k
-transform (Attr _)    _            = error "Expecting object (Attr)"
-transform (Attrs ks)  (A.Object o) = A.Object $ H.filterWithKey (const . (`elem` ks)) o
-transform (Attrs _)   _            = error "Expecting object (Attrs)"
-transform (Obj fs)    x            = A.Object $ H.map (`transform` x) fs
-transform Id          x            = x
+transform :: Builder -> Value       -> Value
+transform Id            x            = x
+transform (At k b)      (A.Object o) = transform b $ o H.! k
+transform (At _ _)      _            = error "Expecting object (At)"
+transform (AtIndex i b) (Array a)    = transform b $ a Vec.! i
+transform (AtIndex _ _) _            = error "Expecting array (AtIndex)"
+transform (Index i)     (Array a)    = a Vec.! i
+transform (Index _)     _            = error "Expecting array (Index)"
+transform (Map b)       (Array a)    = Array $ Vec.map (transform b) a
+transform (Map _)       _            = error "Expecting array (Map)"
+transform (Attr k)      (A.Object o) = o H.! k
+transform (Attr _)      _            = error "Expecting object (Keep)"
+transform (Keep ks)     (A.Object o) = A.Object $ H.filterWithKey (const . (`elem` ks)) o
+transform (Keep _)      _            = error "Expecting object (Keep)"
+transform (Obj fs)      x            = A.Object $ H.map (`transform` x) fs
+transform (Merge a b)   x            = case (transform a x, transform b x) of
+                                       (Object c, Object d) -> Object $ H.union c d
+                                       _ -> error "Expected two objects (Merge)"
 
 -- $use
 --
 -- Filter unwanted attributes from an object
 --
--- > Attrs ["nice", "good"]
+-- > Keep ["nice", "good"]
 -- >
 -- > -- { bad: 3, good: 1, nice: 500, evil: -3 }
 -- > -- => { good: 1, nice: 500 }
@@ -72,15 +80,29 @@ transform Id          x            = x
 --
 -- Extract indices
 --
--- > Map $ Index 0 Id
+-- > Map $ Index 0
 -- >
 -- > -- [[1,2], [3,4]] => [1, 3]
 --
 -- Create object
 --
 -- > Obj $ fromList [
--- >     ("first", Index 0 Id)
--- >   , ("second", Index 1 Id)
+-- >     ("first", Index 0)
+-- >   , ("second", Index 1)
 -- >   ]
 -- >
 -- > -- ["hi", "bye"] => { first:"hi", second:"bye" }
+--
+-- Transform position
+--
+-- > At "ranks" $ AtIndex 0 $ Attr "name"
+-- >
+-- > -- {ranks: [ { name:"winner", score:12 }, { name:"Loser", score: 3 ]}
+-- > -- => "winner"
+--
+-- Combine objects
+--
+-- > Merge (Attr "foo") (Attr "bar")
+-- >
+-- > -- { foo: { a:1, b:2 }, bar: { b:3, c:4 } }
+-- > -- => { a:1, b:3, c:4 }
--- test/Data/Aeson/TransformSpec.hs
+++ test/Data/Aeson/TransformSpec.hs
@@ -25,21 +25,25 @@ spec =
       it "moves builder deeper" $ do
         let obj = [aesonQQ| { foo: { bar: { baz: 1 }}} |]
         transform (At "foo" $ At "bar" Id) obj `shouldBe` [aesonQQ| { baz: 1 } |]
+    context "AtIndex" $
+      it "moves builder deeper" $ do
+        let obj = [aesonQQ| [ [ 1, 2 ] ] |]
+        transform (AtIndex 0 $ AtIndex 1 Id) obj `shouldBe` [aesonQQ| 2 |]
     context "Index" $
       it "moves builder into array" $ do
         let arr = [aesonQQ| [{a:1}, {a:2}, {a:7}] |]
-        transform (Index 1 Id) arr `shouldBe` [aesonQQ| {a:2} |]
+        transform (Index 1) arr `shouldBe` [aesonQQ| {a:2} |]
     context "Attr" $
       it "extracts a value" $ do
         let obj = [aesonQQ| {a:1, b:2} |]
         transform (Attr "a") obj `shouldBe` [aesonQQ| 1 |]
-    context "Attrs" $ do
+    context "Keep" $ do
       it "filters objects by keys" $ do
         let obj = [aesonQQ| {a:1, b:2, c:3} |]
-        transform (Attrs ["a", "b"]) obj `shouldBe` [aesonQQ| {a:1, b:2} |]
+        transform (Keep ["a", "b"]) obj `shouldBe` [aesonQQ| {a:1, b:2} |]
       it "ignores undiscovered keys" $ do
         let obj = [aesonQQ| {a:1, b:2, c:3} |]
-        transform (Attrs ["a", "b", "z"]) obj `shouldBe` [aesonQQ| {a:1, b:2} |]
+        transform (Keep ["a", "b", "z"]) obj `shouldBe` [aesonQQ| {a:1, b:2} |]
     context "Map" $
       it "replaces an array" $ do
         let arr = [aesonQQ| [{a:1}, {a:2}, {a:7}] |]
@@ -51,3 +55,8 @@ spec =
               (Obj $ fromList [("foo", Attr "a") , ("bar", Attr "a")])
               obj
         result `shouldBe` [aesonQQ| {foo: 1, bar: 1} |]
+    context "Merge" $
+      it "combines objects with previous value at a key winning against later value" $ do
+        let obj = [aesonQQ| { foo: {a:1, b:2}, bar: {b:3, c:4} } |]
+        transform  (Merge (Attr "foo") (Attr "bar")) obj `shouldBe`
+          [aesonQQ| { a:1, b:2, c:4 } |]
